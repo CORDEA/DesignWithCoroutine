f78ace26bfb143bedc7753dd64b3eabf51ce4d58
Yoshihiro Tanaka <contact@cordea.jp>
2018-04-30 14:38:50 +0900

    update viewmodel

--- app/src/main/java/jp/cordea/designwithcoroutine/ui/regiondetail/RegionDetailFragment.kt
+++ app/src/main/java/jp/cordea/designwithcoroutine/ui/regiondetail/RegionDetailFragment.kt
@@ -3,8 +3,13 @@ package jp.cordea.designwithcoroutine.ui.regiondetail
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import dagger.android.support.AndroidSupportInjection
 import jp.cordea.designwithcoroutine.api.response.Region
+import jp.cordea.designwithcoroutine.databinding.FragmentRegionDetailBinding
+import javax.inject.Inject
 
 class RegionDetailFragment : Fragment() {
 
@@ -20,6 +25,9 @@ class RegionDetailFragment : Fragment() {
                 }
     }
 
+    @Inject
+    lateinit var viewModel: RegionDetailFragmentViewModel
+
     override fun onAttach(context: Context?) {
         AndroidSupportInjection.inject(this)
         super.onAttach(context)
@@ -27,5 +35,15 @@ class RegionDetailFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel.update(arguments!!.getParcelable(REGION_KEY))
     }
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View =
+            FragmentRegionDetailBinding.inflate(inflater, container, false).apply {
+                vm = viewModel
+            }.root
 }
