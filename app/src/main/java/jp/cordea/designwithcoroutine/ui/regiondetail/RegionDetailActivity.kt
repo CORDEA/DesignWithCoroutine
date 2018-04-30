8e5764b19f4b20d82938702425ea4c65c308c642
Yoshihiro Tanaka <contact@cordea.jp>
2018-04-30 14:38:25 +0900

    add viewpager

--- app/src/main/java/jp/cordea/designwithcoroutine/ui/regiondetail/RegionDetailActivity.kt
+++ app/src/main/java/jp/cordea/designwithcoroutine/ui/regiondetail/RegionDetailActivity.kt
@@ -3,14 +3,19 @@ package jp.cordea.designwithcoroutine.ui.regiondetail
 import android.arch.lifecycle.Observer
 import android.content.Context
 import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import dagger.android.AndroidInjection
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.support.HasSupportFragmentInjector
 import jp.cordea.designwithcoroutine.R
-import kotlinx.android.synthetic.main.activity_region_detail.*
+import jp.cordea.designwithcoroutine.databinding.ActivityRegionDetailBinding
 import javax.inject.Inject
 
-class RegionDetailActivity : AppCompatActivity() {
+class RegionDetailActivity : AppCompatActivity(), HasSupportFragmentInjector {
 
     companion object {
 
@@ -28,15 +33,33 @@ class RegionDetailActivity : AppCompatActivity() {
     @Inject
     lateinit var adapter: RegionDetailFragmentPagerAdapter
 
+    @Inject
+    lateinit var supportFragmentInjector: DispatchingAndroidInjector<Fragment>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_region_detail)
-        setSupportActionBar(toolbar)
+        val binding = DataBindingUtil.setContentView<ActivityRegionDetailBinding>(
+                this,
+                R.layout.activity_region_detail
+        )
+        setSupportActionBar(binding.toolbar)
 
         viewModel.regions.observe(this, Observer {
+            it?.let {
+                adapter.items = it
+            }
         })
 
+        binding.content!!.viewPager.also {
+            it.adapter = adapter
+            it.pageMargin = resources.getDimensionPixelSize(
+                    R.dimen.activity_region_detail_view_pager_margin
+            )
+        }
         viewModel.fetchRegion()
     }
+
+    override fun supportFragmentInjector(): AndroidInjector<Fragment> =
+            supportFragmentInjector
 }
