5ca834a37650474b6bcb5554501cb2674edfe2cc
Yoshihiro Tanaka <contact@cordea.jp>
2018-04-30 14:36:56 +0900

    add layout to display detail of region

--- app/src/main/java/jp/cordea/designwithcoroutine/ui/regiondetail/RegionDetailFragmentModule.kt
+++ app/src/main/java/jp/cordea/designwithcoroutine/ui/regiondetail/RegionDetailFragmentModule.kt
@@ -3,19 +3,11 @@ package jp.cordea.designwithcoroutine.ui.regiondetail
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 import jp.cordea.designwithcoroutine.di.FragmentScope
-import jp.cordea.designwithcoroutine.di.ViewModelModule
 
 @Module
 interface RegionDetailFragmentModule {
 
     @FragmentScope
-    @ContributesAndroidInjector(modules = [
-        RegionDetailFragmentViewModelModule::class
-    ])
+    @ContributesAndroidInjector
     fun contributeRegionDetailFragment(): RegionDetailFragment
 }
-
-@Module
-class RegionDetailFragmentViewModelModule : ViewModelModule<RegionDetailFragmentViewModel>(
-        RegionDetailFragmentViewModel::class
-)
--- app/src/main/java/jp/cordea/designwithcoroutine/ui/regiondetail/RegionDetailFragmentViewModel.kt
+++ app/src/main/java/jp/cordea/designwithcoroutine/ui/regiondetail/RegionDetailFragmentViewModel.kt
@@ -1,10 +1,26 @@
 package jp.cordea.designwithcoroutine.ui.regiondetail
 
-import android.arch.lifecycle.ViewModel
+import android.databinding.ObservableBoolean
+import android.databinding.ObservableField
 import jp.cordea.designwithcoroutine.api.response.Region
+import jp.cordea.designwithcoroutine.di.FragmentScope
+import javax.inject.Inject
 
-class RegionDetailFragmentViewModel : ViewModel() {
+@FragmentScope
+class RegionDetailFragmentViewModel @Inject constructor(
+) {
 
-    lateinit var region: Region
+    val title = ObservableField<String>()
+    val description = ObservableField<String>()
+    val continent = ObservableField<String>()
+    val ddosProtection = ObservableBoolean()
+    val blockStorage = ObservableBoolean()
 
+    fun update(region: Region) {
+        title.set(region.name)
+        description.set(region.country)
+        continent.set(region.continent)
+        ddosProtection.set(region.ddosProtection)
+        blockStorage.set(region.blockStorage)
+    }
 }
